5f0a4d37fc9026658efb803caae8b19cbdd6187c
karvel-code <velgithinji@gmail.com>
2021-03-03 01:06:06 +0300

    Improve game logic

--- lib/gameplay.rb
+++ lib/gameplay.rb
@@ -38,27 +38,14 @@ class GamePlay
                 return 1
             elsif combo.all? {|position| @board[position].eql? 'O'}
                 return 2
-            else
-                return 0
             end
         end
+        0
     end
 
     def check_board
         @board.all? {|element| element.is_a? String}
     end
 
-   
-    # def status
-    #     horizontal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    #     vertical = [[0, 3, 6], [1, 4, 7], [2, 5, 8]]
-    #     diagonal = [[0, 4, 8], [2, 4, 6]]
-    #     winning_combo = [horizontal, vertical, diagonal].flatten(1).freeze
-    #     return_value = check_winner(winning_combo)
-    #     return return_value unless return_value.zero? # Return if someone has won
-    
-    #     return_value = 3 if check_board # Checking for a draw
-    
-    #     return_value
-    # end
+
 end
